d2c0b246a5f1477f9df94f20a5c8918e7947a19f
b1collector <b1collector@gmail.com>
2024-11-20 20:39:13 -0500

    add claude.vim plugin

--- init.lua
+++ init.lua
@@ -899,6 +899,25 @@ require('lazy').setup({
   -- Or use telescope!
   -- In normal mode type `<space>sh` then write `lazy.nvim-plugin`
   -- you can continue same window with `<space>sr` which resumes last telescope search
+
+  -- Claude artificial intelligence
+  {
+    'pasky/claude.vim',
+    lazy = false,
+    config = function()
+      -- Load API key from environment variable
+      local api_key = os.getenv 'ANTHROPIC_API_KEY'
+      if api_key then
+        vim.g.claude_api_key = api_key
+      else
+        vim.notify('ANTHROPIC_API_KEY environment variable is not set', vim.log.levels.WARN)
+      end
+
+      -- Add keymaps (the default conflict with NVChad.  Skip if you want)
+      vim.keymap.set('v', '<leader>Ci', ":'<,'>ClaudeImplement ", { noremap = true, desc = 'Claude Implement' })
+      vim.keymap.set('n', '<leader>Cc', ':ClaudeChat<CR>', { noremap = true, silent = true, desc = 'Claude Chat' })
+    end,
+  },
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
