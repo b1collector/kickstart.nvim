f18ed438513ec6f6029db133ddd910c16890244e
b1collector <b1collector@gmail.com>
2024-11-25 11:11:09 -0500

    add ionide and parrot plugins

--- init.lua
+++ init.lua
@@ -900,24 +900,43 @@ require('lazy').setup({
   -- In normal mode type `<space>sh` then write `lazy.nvim-plugin`
   -- you can continue same window with `<space>sr` which resumes last telescope search
 
-  -- Claude artificial intelligence
-  {
-    'pasky/claude.vim',
-    lazy = false,
-    config = function()
-      -- Load API key from environment variable
-      local api_key = os.getenv 'ANTHROPIC_API_KEY'
-      if api_key then
-        vim.g.claude_api_key = api_key
-      else
-        vim.notify('ANTHROPIC_API_KEY environment variable is not set', vim.log.levels.WARN)
-      end
+  { 'ionide/Ionide-vim', ft = 'fsharp' },
 
-      -- Add keymaps (the default conflict with NVChad.  Skip if you want)
-      vim.keymap.set('v', '<leader>Ci', ":'<,'>ClaudeImplement ", { noremap = true, desc = 'Claude Implement' })
-      vim.keymap.set('n', '<leader>Cc', ':ClaudeChat<CR>', { noremap = true, silent = true, desc = 'Claude Chat' })
+  -- generic ai plugin
+  {
+    'frankroeder/parrot.nvim',
+    dependencies = { 'ibhagwan/fzf-lua', 'nvim-lua/plenary.nvim' },
+    opts = {},
+    config = function()
+      require('parrot').setup {
+        -- Providers must be explicitly added to make them available.
+        providers = {
+          anthropic = {
+            api_key = os.getenv 'ANTHROPIC_API_KEY',
+          },
+        },
+      }
     end,
   },
+
+  -- Claude artificial intelligence
+  -- {
+  --   'pasky/claude.vim',
+  --   lazy = false,
+  --   config = function()
+  --     -- Load API key from environment variable
+  --     local api_key = os.getenv 'ANTHROPIC_API_KEY'
+  --     if api_key then
+  --       vim.g.claude_api_key = api_key
+  --     else
+  --       vim.notify('ANTHROPIC_API_KEY environment variable is not set', vim.log.levels.WARN)
+  --     end
+  --
+  --     -- Add keymaps (the default conflict with NVChad.  Skip if you want)
+  --     vim.keymap.set('v', '<leader>Ci', ":'<,'>ClaudeImplement ", { noremap = true, desc = 'Claude Implement' })
+  --     vim.keymap.set('n', '<leader>Cc', ':ClaudeChat<CR>', { noremap = true, silent = true, desc = 'Claude Chat' })
+  --   end,
+  -- },
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
