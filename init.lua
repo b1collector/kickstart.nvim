b913abd603d89924b40c3d3b30826bcb3641181d
Jason Hursey <jhursey@connorsllc.com>
2025-07-18 09:59:27 -0400

    remove parrot plugin

--- init.lua
+++ init.lua
@@ -925,22 +925,6 @@ require('lazy').setup({
 
   { 'ionide/Ionide-vim', ft = 'fsharp' },
 
-  -- generic ai plugin
-  {
-    'frankroeder/parrot.nvim',
-    dependencies = { 'ibhagwan/fzf-lua', 'nvim-lua/plenary.nvim' },
-    opts = {},
-    config = function()
-      require('parrot').setup {
-        -- Providers must be explicitly added to make them available.
-        providers = {
-          anthropic = {
-            api_key = os.getenv 'ANTHROPIC_API_KEY',
-          },
-        },
-      }
-    end,
-  },
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
